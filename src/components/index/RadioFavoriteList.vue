9a5abd1a39c2f98519d5f82e678093907f794f8e
adriel-mp3 <contatoadrielsantos@hotmail.com>
2025-02-16 20:20:22 -0300

    feat(components/index): add favorite radio list component

---
+++ src/components/index/RadioFavoriteList.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type { RadioStation } from "@/types/radio";
+import { inject } from "vue";
+import RadioFavoriteCard from "./RadioFavoriteCard.vue";
+
+defineProps<{
+  radios: RadioStation[];
+}>();
+
+const playingRadioStation = inject<RadioStation | null>("playingRadioStation");
+</script>
+
+<template>
+  <ul class="space-y-3 pr-4">
+    <li v-for="radio in radios" :key="radio.stationuuid">
+      <RadioFavoriteCard
+        :radio="radio"
+        :is-playing="playingRadioStation?.stationuuid === radio.stationuuid"
+      />
+    </li>
+  </ul>
+</template>
