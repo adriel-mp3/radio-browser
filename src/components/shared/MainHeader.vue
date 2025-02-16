51e4354824cf7be0add1b691e0a5902074dd6820
adriel-mp3 <contatoadrielsantos@hotmail.com>
2025-02-16 19:44:41 -0300

    feat(compontents/shared): add header

---
+++ src/components/shared/MainHeader.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { defineEmits } from "vue";
+
+const emit = defineEmits(["toggle-sidebar"]);
+</script>
+
+<template>
+  <header class="p-4 bg-gray-800 flex justify-between items-center">
+    <button @click="emit('toggle-sidebar')" class="text-xl md:hidden">☰</button>
+    <h1 class="text-xl">Radio Browser</h1>
+  </header>
+</template>
