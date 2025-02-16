4896dff1839e69b4c4f9b8f63ca263dc9cec67f6
adriel-mp3 <contatoadrielsantos@hotmail.com>
2025-02-16 20:15:15 -0300

    feat:(components): add base radio card component

---
+++ src/components/RadioCard.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="w-full bg-gray-600 p-3 rounded flex items-center justify-between">
+    <div>
+      <slot name="label"></slot>
+    </div>
+
+    <div class="flex-1 mx-4">
+      <slot></slot>
+    </div>
+
+    <div class="flex gap-3">
+      <slot name="actions"></slot>
+    </div>
+  </div>
+</template>
