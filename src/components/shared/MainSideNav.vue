6857f91c3cdf458958f9aab1ac63240bd5886996
adriel-mp3 <contatoadrielsantos@hotmail.com>
2025-02-16 19:44:52 -0300

    feat(compontents/shared): add sidenav

---
+++ src/components/shared/MainSideNav.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { defineProps, defineEmits } from "vue";
+
+defineProps<{ isSidebarOpen: boolean }>();
+const emit = defineEmits(["toggle-sidebar"]);
+</script>
+
+<template>
+  <aside
+    v-if="isSidebarOpen"
+    class="bg-gray-800 p-4 flex flex-col fixed inset-y-0 left-0 transition-all duration-300 md:relative md:w-64 z-50 overflow-y-auto"
+  >
+    <button
+      class="text-xl mb-4 md:hidden self-end"
+      @click="emit('toggle-sidebar')"
+    >
+      ✖
+    </button>
+    <slot />
+  </aside>
+</template>
